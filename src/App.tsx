5ad0de254c5efa8b5ad6013cb3f9e0551d40b5ca
Mariusz <mar.lewandowski@gmail.com>
2022-05-13 20:19:28 +0200

    beginning styling header and little html

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import './App.css'
 
 export const App = () => {
 
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <>
+            <header>
+                <h1>
+                    <strong>Mega </strong> Ogłoszenia
+                </h1>
+                <button>Dodaj ogłoszenie</button>
+                <div className="search">
+                    <input type="text"/><button>Szukaj</button>
+                </div>
+            </header>
+            <div className='map'>
+                ...
+            </div>
+        </>
+    );
 }
