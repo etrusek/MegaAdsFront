b94c1ffc26ae4a46fd9191b88fa72755d6d74547
RoJinx <mar.lewandowski@gmail.com>
2022-05-25 15:15:12 +0200

    added Search to work a little in FE and betwin components witch SearchContext

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {EventHandler, FormEvent, FormEventHandler, SyntheticEvent, useContext, useState} from 'react';
 import './Header.css'
 import {Btn} from "../common/Btn";
+import {SearchContext} from "../../context/search.context";
 
-export const Header = () =>  (
+export const Header = () => {
+    const {search, setSearch} = useContext(SearchContext)
+    const [inputVal, setInputVal] = useState(search);
+
+    const setSearchFromLocaleState = (e: SyntheticEvent) => {
+        e.preventDefault();
+        setSearch(inputVal);
+    }
+
+    return (
         <header>
             <h1>
                 <strong>Mega </strong> Ogłoszenia
             </h1>
             <Btn text='Dodaj ogłoszenie'/>
-            <div className="search">
-                <input type="text"/>
+            <form className="search" onSubmit={setSearchFromLocaleState}>
+                <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
                 <Btn text='Szukaj'/>
 
-            </div>
+            </form>
         </header>
     )
+}
